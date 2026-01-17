20c72e9207545049b984a68164a7d7de7bc067bb
Yaman Gahlout <yamangahlout123@gmail.com>
2026-01-17 21:59:26 +0530

    email error fixed

--- utils/mail.js
+++ utils/mail.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  service: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: "yamangahlout19@gmail.com",
     pass: "sktaiikczmpgrclj",
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
 
 const sendMail = async (name, email, subject, message) => {
   const info = await transporter.sendMail({
-    from: email,
+    from: '"Yaman Gahlout" <yamangahlout19@gmail.com>', // sender address
     to: "yamangahlout123@gmail.com",
     subject: subject,
     html: `<p>${message}</p> <h4>Name: ${name}</h4><h4>Email : ${email}</h4>`, // HTML body
